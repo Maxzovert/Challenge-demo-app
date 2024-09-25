f2f0e18a19692fac22e58588e38c0da292ff0de8
Abdullah <134436822+Maxzovert@users.noreply.github.com>
2024-09-25 20:55:25 +0530

    setup and useref use

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState , useRef } from "react";
+
+export default function Player() {
+
+  const playerName = useRef();
+  const [getplayerName , setPlayerName] = useState(null);
+
+  function handleClick(){
+    setPlayerName(playerName.current.value)
+    playerName.current.value = ''; 
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {getplayerName ?? 'unknown'} !!</h2>
+      <p>
+        <input ref={playerName}
+         type="text"
+         />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
